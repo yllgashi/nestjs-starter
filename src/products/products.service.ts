59c970bb699637f226654a917b1c1dc9f3e4ef42
yllgashi <yllxgashi@gmail.com>
2022-02-26 03:01:27 +0100

    call stored procedure from service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -9,9 +9,23 @@ export class ProductsService {
   constructor(private connection: Connection) {}
 
   async getProducts(): Promise<Product[]> {
-    let data = await this.connection.query('SELECT * FROM Gastronomy.Product');
+    let data = await this.connection.query('EXEC Gastronomy.usp_Product_GetAll');
+
     return data
   }
+
+  // async getProducts(): Promise<Product[]> {
+  //   const queryRunner = this.connection.createQueryRunner();
+
+  //   // you can use its methods only after you call connect
+  //   // which performs real database connection
+  //   await queryRunner.connect();
+
+  //   // .. now you can work with query runner and call its methods
+
+  //   // very important - don't forget to release query runner once you finished working with it
+  //   await queryRunner.release();
+  // }
 
   createProduct(product: Product) {
     return;
